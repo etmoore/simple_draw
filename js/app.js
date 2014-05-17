4f28d9577389e989a5cb4e79fa0918b365a67ea1
Evan Moore <etmoore@gmail.com>
2014-05-16 22:32:25 -0600

    added drawing ability to canvas

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 //Problem: No user interaction capability
 //Solution: When user interacts, cause changes appropriately
 var color = $(".selected").css("background-color");;
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 
 //When clicking on control list items
-$(".controls li").click(function(){
+$(".controls").on("click", "li", function(){
   //Deselect sibling elements
   $(this).siblings().removeClass("selected");
   //Select clicked element
@@ -42,6 +46,22 @@ $("#addNewColor").click(function(){
   $newColor.click();
 });
 
-
 //On mouse events on the canvas
+$canvas.mousedown(function(e){
+  lastEvent = e;
+   mouseDown = true;
+}).mousemove(function(e){
   //Draw lines
+  if(mouseDown) {
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function(){
+  mouseDown = false;
+}).mouseleave(function(){
+  $canvas.mouseup();
+});
